11f42e3eb64f4b40e0aed31c1627c352ff082e01
Namean <dwilsoncnc@gmail.com>
2021-03-14 20:17:43 -0400

    I was able to create a POST route for express to return a response and print to the console "API is working correctly", I also added a timestamp to the server output for Nodemon reloads

--- server.js
+++ server.js
@@ -19,12 +19,19 @@ app.get('/', (req, res) => {
   res.send('API is working correctly!')
 })
 
+app.post('/data', (req, res) => {
+	console.log('API is working correctly');
+	res.send('API is working correctly')
+})
+
 // DATABASE: SQLite3
 
 
 
 
 // SERVER
+
+let timestamp = new Date().toLocaleTimeString()
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port} \n @${timestamp}`)
 })
