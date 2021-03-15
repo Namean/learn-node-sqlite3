783bad25fb143f87fac8c18ea10b30c9357c5903
Namean <dwilsoncnc@gmail.com>
2021-03-14 20:01:42 -0400

    Cleaned up the server.js file to look cleaner

--- server.js
+++ server.js
@@ -1,71 +1,30 @@
 const express = require('express')
 const app = express()
-const port = 3000
 const fs = require('fs')
 const bodyParser = require("body-parser");
-const cors = require('cors');
-// CONFIG
 
+// CONFIG: APPLICATION
+const port = 3000
 
+// CONFIG: EXPRESS
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors);
 
-
+// STATIC FILES
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
-
-
-
-
-// database
-var sqlite3 = require('sqlite3').verbose();
-databaseName = "./chinook.db"
-var db = new sqlite3.Database(databaseName);
-databaseQuery = "SELECT Name, Composer from tracks LIMIT 10;"
-
-
-
-// ROUTES
-// app.get('/', (req, res) => {
-//   res.send('API is working correctly!')
-// })
-
-
-app.get("/", (request, response) => {
-  response.sendFile(`${__dirname}/views/index.html`);
-});
-
-app.post('/', function (req, res) {
-  res.send('Got a POST request')
+// ROUTES: EXPRESS
+app.get('/', (req, res) => {
+  res.send('API is working correctly!')
 })
 
+// DATABASE: SQLite3
 
 
 
 
-// app.get('/sqlite3', function (req, res) {
-// 	db.serialize(function() {
-// 	  // db.run(databaseQuery);
-// 	  db.each(`${databaseQuery}`, function(err, row) {
-//       console.log(row.Composer + ' : ' + row.Name);
-      
-//       res.send(row);
-//   });
-// });
-
-
-
-
-app.put('/user', function (req, res) {
-  res.send('Got a PUT request at /user')
-})
-
-app.delete('/user', function (req, res) {
-  res.send('Got a DELETE request at /user')
-})
-
+// SERVER
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
